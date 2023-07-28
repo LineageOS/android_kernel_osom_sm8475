mikeNG/android_kernel_qcom_sm8450

power: qti_battery_charger: Support wireless charger
Change-Id: I90843c790b157fbd28c34265ca03a070a7c272b3
Signed-off-by: Luca Stefani <luca@osomprivacy.com>

drivers/power/supply/qti_battery_charger.c
@@ -28,6 +28,7 @@
 #define MSG_TYPE_NOTIFY			2
 
 /* opcode for battery charger */
+#define BC_GET_BATT_ID_REQ		0x00
 #define BC_SET_NOTIFY_REQ		0x04
 #define BC_DISABLE_NOTIFY_REQ		0x05
 #define BC_NOTIFY_IND			0x07
@@ -38,6 +39,7 @@
 #define BC_WLS_STATUS_GET		0x34
 #define BC_WLS_STATUS_SET		0x35
 #define BC_SHIP_MODE_REQ_SET		0x36
+#define BC_GET_OTG_STATUS_REQ		0x39
 #define BC_WLS_FW_CHECK_UPDATE		0x40
 #define BC_WLS_FW_PUSH_BUF_REQ		0x41
 #define BC_WLS_FW_UPDATE_STATUS_RESP	0x42
@@ -201,6 +203,20 @@ struct wireless_fw_get_version_resp {
 	u32			fw_version;
 };
 
+struct batt_mngr_get_batt_id_req {
+	struct pmic_glink_hdr	hdr;
+};
+
+struct batt_mngr_get_batt_id_resp{
+	struct pmic_glink_hdr	hdr;
+	u32			battery_id;
+};
+
+struct batt_mngr_get_otg_status_resp{
+	struct pmic_glink_hdr	hdr;
+	bool		otg_id;
+};
+
 struct battery_charger_ship_mode_req_msg {
 	struct pmic_glink_hdr	hdr;
 	u32			ship_mode_type;
@@ -255,6 +271,8 @@ struct battery_chg_dev {
 	/* To track the driver initialization status */
 	bool				initialized;
 	bool				notify_en;
+	u32					batter_id;
+	bool				otg_status;
 };
 
 static const int battery_prop_map[BATT_PROP_MAX] = {
@@ -572,6 +590,8 @@ static void handle_message(struct battery_chg_dev *bcdev, void *data,
 	struct wireless_fw_push_buf_resp *fw_resp_msg;
 	struct wireless_fw_update_status *fw_update_msg;
 	struct wireless_fw_get_version_resp *fw_ver_msg;
+	struct batt_mngr_get_batt_id_resp *batt_id_msg;
+	struct batt_mngr_get_otg_status_resp *otg_status_msg;
 	struct psy_state *pst;
 	bool ack_set = false;
 
@@ -666,6 +686,26 @@ static void handle_message(struct battery_chg_dev *bcdev, void *data,
 		if (len == sizeof(*fw_ver_msg)) {
 			fw_ver_msg = data;
 			bcdev->wls_fw_version = fw_ver_msg->fw_version;
+			ack_set = true;
+		} else {
+			pr_err("Incorrect response length %zu for wls_fw_get_version\n",
+				len);
+		}
+		break;
+	case BC_GET_BATT_ID_REQ:
+		if (len == sizeof(*batt_id_msg)) {
+			batt_id_msg = data;
+			bcdev->batter_id = batt_id_msg->battery_id;
+			ack_set = true;
+		} else {
+			pr_err("Incorrect response length %zu for wls_fw_get_version\n",
+				len);
+		}
+		break;
+	case BC_GET_OTG_STATUS_REQ:
+		if (len == sizeof(*otg_status_msg)) {
+			otg_status_msg = data;
+			bcdev->otg_status = otg_status_msg->otg_id;
 			ack_set = true;
 		} else {
 			pr_err("Incorrect response length %zu for wls_fw_get_version\n",
@@ -1384,7 +1424,6 @@ static int wireless_fw_check_for_update(struct battery_chg_dev *bcdev,
 	req_msg.fw_version = version;
 	req_msg.fw_size = size;
 	req_msg.fw_crc = bcdev->wls_fw_crc;
-
 	return battery_chg_write(bcdev, &req_msg, sizeof(req_msg));
 }
 
@@ -1562,6 +1601,50 @@ static ssize_t wireless_fw_version_show(struct class *c,
 }
 static CLASS_ATTR_RO(wireless_fw_version);
 
+static ssize_t battery_id_show(struct class *c,
+					struct class_attribute *attr,
+					char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct batt_mngr_get_batt_id_req req_msg = {};
+	int rc;
+
+	req_msg.hdr.owner = MSG_OWNER_BC;
+	req_msg.hdr.type = MSG_TYPE_REQ_RESP;
+	req_msg.hdr.opcode = BC_GET_BATT_ID_REQ;
+        
+	rc = battery_chg_write(bcdev, &req_msg, sizeof(req_msg));
+	if (rc < 0) {
+		pr_err("Failed to get battery id rc=%d\n", rc);
+		return rc;
+	}
+	return scnprintf(buf, PAGE_SIZE, "%d\n", bcdev->batter_id);
+}
+static CLASS_ATTR_RO(battery_id);
+
+static ssize_t otg_status_show(struct class *c,
+					struct class_attribute *attr,
+					char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct batt_mngr_get_batt_id_req req_msg = {};
+	int rc;
+
+	req_msg.hdr.owner = MSG_OWNER_BC;
+	req_msg.hdr.type = MSG_TYPE_REQ_RESP;
+	req_msg.hdr.opcode = BC_GET_OTG_STATUS_REQ;
+        
+	rc = battery_chg_write(bcdev, &req_msg, sizeof(req_msg));
+	if (rc < 0) {
+		pr_err("Failed to get otg_status rc=%d\n", rc);
+		return rc;
+	}
+	return scnprintf(buf, PAGE_SIZE, "%d\n", bcdev->otg_status);
+}
+static CLASS_ATTR_RO(otg_status);
+
 static ssize_t wireless_fw_force_update_store(struct class *c,
 					struct class_attribute *attr,
 					const char *buf, size_t count)
@@ -1594,13 +1677,33 @@ static ssize_t wireless_fw_update_store(struct class *c,
 	if (kstrtobool(buf, &val) || !val)
 		return -EINVAL;
 
+	rc = wireless_fw_check_for_update(bcdev, 1, 1);
+	if (rc < 0)
+		return rc;
+
 	rc = wireless_fw_update(bcdev, false);
 	if (rc < 0)
 		return rc;
 
 	return count;
 }
-static CLASS_ATTR_WO(wireless_fw_update);
+
+static ssize_t wireless_fw_update_show(struct class *c,
+					struct class_attribute *attr, char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	int rc;
+
+    rc = wireless_fw_check_for_update(bcdev, 1, 1);
+	if (rc < 0)
+		return rc;
+	
+	return scnprintf(buf, PAGE_SIZE, "%d\n", rc);
+	
+}
+
+static CLASS_ATTR_RW(wireless_fw_update);
 
 static ssize_t usb_typec_compliant_show(struct class *c,
 				struct class_attribute *attr, char *buf)
@@ -1891,6 +1994,8 @@ static struct attribute *battery_class_attrs[] = {
 	&class_attr_restrict_cur.attr,
 	&class_attr_usb_real_type.attr,
 	&class_attr_usb_typec_compliant.attr,
+	&class_attr_battery_id.attr,
+	&class_attr_otg_status.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(battery_class);

LineageOS/android_kernel_osom_sm8475

drivers/power/supply/qti_battery_charger.c
@@ -30,6 +30,7 @@
 #define MSG_TYPE_NOTIFY			2
 
 /* opcode for battery charger */
+#define BC_GET_BATT_ID_REQ		0x00
 #define BC_SET_NOTIFY_REQ		0x04
 #define BC_DISABLE_NOTIFY_REQ		0x05
 #define BC_NOTIFY_IND			0x07
@@ -40,6 +41,7 @@
 #define BC_WLS_STATUS_GET		0x34
 #define BC_WLS_STATUS_SET		0x35
 #define BC_SHIP_MODE_REQ_SET		0x36
+#define BC_GET_OTG_STATUS_REQ		0x39
 #define BC_WLS_FW_CHECK_UPDATE		0x40
 #define BC_WLS_FW_PUSH_BUF_REQ		0x41
 #define BC_WLS_FW_UPDATE_STATUS_RESP	0x42
@@ -211,6 +213,20 @@ struct wireless_fw_get_version_resp {
 	u32			fw_version;
 };
 
+struct batt_mngr_get_batt_id_req {
+	struct pmic_glink_hdr	hdr;
+};
+
+struct batt_mngr_get_batt_id_resp{
+	struct pmic_glink_hdr	hdr;
+	u32			battery_id;
+};
+
+struct batt_mngr_get_otg_status_resp{
+	struct pmic_glink_hdr	hdr;
+	bool		otg_id;
+};
+
 struct battery_charger_ship_mode_req_msg {
 	struct pmic_glink_hdr	hdr;
 	u32			ship_mode_type;
@@ -272,6 +288,8 @@ struct battery_chg_dev {
 	bool				initialized;
 	bool				notify_en;
 	bool				error_prop;
+	u32				battery_id;
+	bool				otg_status;
 };
 
 static const int battery_prop_map[BATT_PROP_MAX] = {
@@ -664,6 +682,8 @@ static void handle_message(struct battery_chg_dev *bcdev, void *data,
 	struct wireless_fw_push_buf_resp *fw_resp_msg;
 	struct wireless_fw_update_status *fw_update_msg;
 	struct wireless_fw_get_version_resp *fw_ver_msg;
+	struct batt_mngr_get_batt_id_resp *batt_id_msg;
+	struct batt_mngr_get_otg_status_resp *otg_status_msg;
 	struct psy_state *pst;
 	bool ack_set = false;
 
@@ -758,6 +778,26 @@ static void handle_message(struct battery_chg_dev *bcdev, void *data,
 		if (len == sizeof(*fw_ver_msg)) {
 			fw_ver_msg = data;
 			bcdev->wls_fw_version = fw_ver_msg->fw_version;
+			ack_set = true;
+		} else {
+			pr_err("Incorrect response length %zu for wls_fw_get_version\n",
+				len);
+		}
+		break;
+	case BC_GET_BATT_ID_REQ:
+		if (len == sizeof(*batt_id_msg)) {
+			batt_id_msg = data;
+			bcdev->battery_id = batt_id_msg->battery_id;
+			ack_set = true;
+		} else {
+			pr_err("Incorrect response length %zu for wls_fw_get_version\n",
+				len);
+		}
+		break;
+	case BC_GET_OTG_STATUS_REQ:
+		if (len == sizeof(*otg_status_msg)) {
+			otg_status_msg = data;
+			bcdev->otg_status = otg_status_msg->otg_id;
 			ack_set = true;
 		} else {
 			pr_err("Incorrect response length %zu for wls_fw_get_version\n",
@@ -1724,6 +1764,50 @@ static ssize_t wireless_fw_version_show(struct class *c,
 }
 static CLASS_ATTR_RO(wireless_fw_version);
 
+static ssize_t battery_id_show(struct class *c,
+					struct class_attribute *attr,
+					char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct batt_mngr_get_batt_id_req req_msg = {};
+	int rc;
+
+	req_msg.hdr.owner = MSG_OWNER_BC;
+	req_msg.hdr.type = MSG_TYPE_REQ_RESP;
+	req_msg.hdr.opcode = BC_GET_BATT_ID_REQ;
+
+	rc = battery_chg_write(bcdev, &req_msg, sizeof(req_msg));
+	if (rc < 0) {
+		pr_err("Failed to get battery id rc=%d\n", rc);
+		return rc;
+	}
+	return scnprintf(buf, PAGE_SIZE, "%d\n", bcdev->battery_id);
+}
+static CLASS_ATTR_RO(battery_id);
+
+static ssize_t otg_status_show(struct class *c,
+					struct class_attribute *attr,
+					char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct batt_mngr_get_batt_id_req req_msg = {};
+	int rc;
+
+	req_msg.hdr.owner = MSG_OWNER_BC;
+	req_msg.hdr.type = MSG_TYPE_REQ_RESP;
+	req_msg.hdr.opcode = BC_GET_OTG_STATUS_REQ;
+
+	rc = battery_chg_write(bcdev, &req_msg, sizeof(req_msg));
+	if (rc < 0) {
+		pr_err("Failed to get otg_status rc=%d\n", rc);
+		return rc;
+	}
+	return scnprintf(buf, PAGE_SIZE, "%d\n", bcdev->otg_status);
+}
+static CLASS_ATTR_RO(otg_status);
+
 static ssize_t wireless_fw_force_update_store(struct class *c,
 					struct class_attribute *attr,
 					const char *buf, size_t count)
@@ -1756,13 +1840,32 @@ static ssize_t wireless_fw_update_store(struct class *c,
 	if (kstrtobool(buf, &val) || !val)
 		return -EINVAL;
 
+	rc = wireless_fw_check_for_update(bcdev, 1, 1);
+	if (rc < 0)
+		return rc;
+
 	rc = wireless_fw_update(bcdev, false);
 	if (rc < 0)
 		return rc;
 
 	return count;
 }
-static CLASS_ATTR_WO(wireless_fw_update);
+
+static ssize_t wireless_fw_update_show(struct class *c,
+					struct class_attribute *attr, char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	int rc;
+
+    rc = wireless_fw_check_for_update(bcdev, 1, 1);
+	if (rc < 0)
+		return rc;
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", rc);
+}
+
+static CLASS_ATTR_RW(wireless_fw_update);
 
 static ssize_t usb_typec_compliant_show(struct class *c,
 				struct class_attribute *attr, char *buf)
@@ -2070,6 +2173,8 @@ static struct attribute *battery_class_attrs[] = {
 	&class_attr_restrict_cur.attr,
 	&class_attr_usb_real_type.attr,
 	&class_attr_usb_typec_compliant.attr,
+	&class_attr_battery_id.attr,
+	&class_attr_otg_status.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(battery_class);